b897075d7ac625e3d8da72e421ea754d5b1027a7
IbrahimZRehmani <IbrahimZubairRehmani@gmail.com>
2026-07-06 18:57:12 +0100

    Two Part Assignment
    Two Part Assignment Completion

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,1 +1,6 @@
-alert("hello,world!")
+function My_First_Function() {
+		var String = "kiss me, im irish!";
+		var result = String.fontcolour("green");
+		document.getElementById("irish").innerHTML = 
+		result;
+	}
